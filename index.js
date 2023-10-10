faea3ae37c20064d9592fd7074d077b3a4dc09f5
Hugo Rizzo <rizzo_hugo@hotmail.com>
2023-10-10 13:44:58 -0300

    att

---
+++ index.js
@@ -0,0 +1,12 @@
+console.log('Meu nome é "Hugo". Estou aprendendo JavaScript às', 10, 'da manhã')
+
+let nome;
+nome = 'Cleiton';
+nomeEmpresa = 'ifood';
+cargoNovo = 'programação'
+empresaNova = 'Nvidia'
+
+console.log(nome, 'trabalha como entregador, na empresa', nomeEmpresa, 'mas recentemente ele esta querendo aprimorar seus conhecimentos para', cargoNovo, 'e entrar na empresa', empresaNova, 'para ter uma melhor qualidade de vida');
+
+
+
