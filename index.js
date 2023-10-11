feb5e3ddaca9ec5abffd3524d8476979976e8828
Hugo Rizzo <rizzo_hugo@hotmail.com>
2023-10-11 13:46:33 -0300

    Update index.js

--- index.js
+++ index.js
@@ -9,4 +9,48 @@ empresaNova = 'Nvidia'
 console.log(nome, 'trabalha como entregador, na empresa', nomeEmpresa, 'mas recentemente ele esta querendo aprimorar seus conhecimentos para', cargoNovo, 'e entrar na empresa', empresaNova, 'para ter uma melhor qualidade de vida');
 
 
+//------------------------------------------------
+
+const primeiroNumero = 5
+const segundoNumero = 15
+
+const conta1 = primeiroNumero * segundoNumero;
+const conta2 = primeiroNumero + segundoNumero;
+const conta3 = primeiroNumero - segundoNumero;
+const conta4 = primeiroNumero / segundoNumero;
+
+console.log(conta1, conta2, conta3, conta4)
+
+
+//Exercicio
+
+const nome1 = 'Hugo'
+const sobrenome = 'Rizzo'
+const idade = 30
+const peso = 84
+const altura = 1.80
+
+let imc
+
+indiceDeMassaCorporal = 84 / (altura * altura);
+
+console.log(indiceDeMassaCorporal)
+
+let anoNascimento
+
+anoNascimento = (2023 - idade)
+
+console.log(anoNascimento)
+
+
+console.log(nome1, sobrenome, 'tem', idade, 'anos, pesa', peso, 'kg')
+console.log('tem', altura, 'de altura e seu IMC é de', indiceDeMassaCorporal)
+console.log(nome, 'nasceu em', anoNascimento, '.')
+
+//template strings
+
+console.log(`${nome1} ${sobrenome} tem ${idade} anos pesa ${peso} kg`)
+
+
+
 
