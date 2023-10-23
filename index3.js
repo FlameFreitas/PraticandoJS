ac85bb8a186833c7ed37d65c92d06239d9805a9d
Hugo Rizzo <rizzo_hugo@hotmail.com>
2023-10-23 17:59:52 -0300

    Att

--- index3.js
+++ index3.js
@@ -1,13 +1,13 @@
 //Operação ternária
 
-const pontosUsuário = 999   
+const pontosUsuário = 999
 
 const nivelUsuário = pontosUsuário >= 1000 ? 'Usuário VIP' : 'Usuário normal'
 let corUsuário
 
-if(pontosUsuário >= 1000){
+if (pontosUsuário >= 1000) {
     corUsuário = 'Color: Gold'
-}else{
+} else {
     corUsuário = 'Color: White'
 }
 
@@ -22,23 +22,23 @@ console.log(nivelUsuário, corUsuário)
 
 //const data2 = new Date('2023-05-15 11:03:50:100')
 
-function zeroAEsquerda(num){
-    return num >=10 ? num : `0${num}`
+function zeroAEsquerda(num) {
+    return num >= 10 ? num : `0${num}`
 }
 
-function formataData(data){
+function formataData(data) {
 
-const ano = zeroAEsquerda(data.getFullYear());
-const mes = zeroAEsquerda(data.getMonth()+ 1);
-const dia = zeroAEsquerda(data.getDate());
-const hora = zeroAEsquerda(data.getHours());
-const minutos = zeroAEsquerda(data.getMinutes());
-const segundos = zeroAEsquerda(data.getSeconds());
-const MS = zeroAEsquerda(data.getMilliseconds());
-const diaSemana = zeroAEsquerda(data.getDay()); // 0 - Domingo - 6 - Sabado
+    const ano = zeroAEsquerda(data.getFullYear());
+    const mes = zeroAEsquerda(data.getMonth() + 1);
+    const dia = zeroAEsquerda(data.getDate());
+    const hora = zeroAEsquerda(data.getHours());
+    const minutos = zeroAEsquerda(data.getMinutes());
+    const segundos = zeroAEsquerda(data.getSeconds());
+    const MS = zeroAEsquerda(data.getMilliseconds());
+    const diaSemana = zeroAEsquerda(data.getDay()); // 0 - Domingo - 6 - Sabado
 
 
-return `${ano}/${mes}/${dia}   ${hora}:${minutos}:${segundos}:${MS} e Dia da semana ${diaSemana}`
+    return `${ano}/${mes}/${dia}   ${hora}:${minutos}:${segundos}:${MS} e Dia da semana ${diaSemana}`
 
 }
 
@@ -108,38 +108,41 @@ let diaDaSemanaTexto
 
 //Agora em função
 
-function getWeekText (diaSemana){
+function getWeekText(diaSemana) {
     let diaDaSemanaTexto
 
-switch (diaSemana){
-     case 0:
-         diaDaSemanaTexto = 'Domingo'
-         break
-     case 1:
-         diaDaSemanaTexto = 'Segunda'
-         break
-     case 2:
-         diaDaSemanaTexto = 'Terça'
-         break
-     case 3:
-         diaDaSemanaTexto = 'Quarta'
-         break
-     case 4:
-         diaDaSemanaTexto = 'Quinta'
-         break
-     case 5:
-         diaDaSemanaTexto = 'Sexta'
-         break
-     case 6:
-         diaDaSemanaTexto = 'Sábado'
-         break
-         default:
-             diaDaSemanaTexto = ''
-             break
-}
-    
+    switch (diaSemana) {
+        case 0:
+            diaDaSemanaTexto = 'Domingo'
+            return diaDaSemanaTexto
+        case 1:
+            diaDaSemanaTexto = 'Segunda'
+            return diaDaSemanaTexto
+        case 2:
+            diaDaSemanaTexto = 'Terça'
+            return diaDaSemanaTexto
+        case 3:
+            diaDaSemanaTexto = 'Quarta'
+            return diaDaSemanaTexto
+        case 4:
+            diaDaSemanaTexto = 'Quinta'
+            return diaDaSemanaTexto
+        case 5:
+            diaDaSemanaTexto = 'Sexta'
+            return diaDaSemanaTexto
+        case 6:
+            diaDaSemanaTexto = 'Sábado'
+            return diaDaSemanaTexto
+        default:
+            diaDaSemanaTexto = ''
+            return diaDaSemanaTexto
+    }
+
 }
 
-console.log(diaSemana, diaDaSemanaTexto)
+dayFromWeekText = getWeekText(diaSemana)
+
+
+console.log(diaSemana, dayFromWeekText)
 
 
