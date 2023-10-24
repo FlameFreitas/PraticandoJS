be680889bb216b1c2dfb870146889230999c4854
Hugo Rizzo <rizzo_hugo@hotmail.com>
2023-10-24 14:32:10 -0300

    Update js.js

--- ExercicioFor/js.js
+++ ExercicioFor/js.js
@@ -1,8 +1,19 @@
-const elementros = [
+const elementos = [
     { tag: 'p', texto: 'Frase 1' },
     { tag: 'div', texto: 'Frase 2' },
     { tag: 'section', texto: 'Frase 3' },
     { tag: 'footer', texto: 'Frase 4' }
 
 ]
+
+const container = document.querySelector('.container')
+
+const div = document.createElement('div')
+
+let i
+
+for (i = 0; i < elementos.length(); i++) {
+    console.log(elementos[i])
+}
+
 
