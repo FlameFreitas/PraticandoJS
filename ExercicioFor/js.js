d30844fdc0d29f78b862fdd99c071d1139bb1c58
Hugo Rizzo <rizzo_hugo@hotmail.com>
2023-10-24 14:27:39 -0300

    att

---
+++ ExercicioFor/js.js
@@ -0,0 +1,8 @@
+const elementros = [
+    { tag: 'p', texto: 'Frase 1' },
+    { tag: 'div', texto: 'Frase 2' },
+    { tag: 'section', texto: 'Frase 3' },
+    { tag: 'footer', texto: 'Frase 4' }
+
+]
+
