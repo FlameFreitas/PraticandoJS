0e30cd970a34be140eac4a5df63b4c8cf10c2aca
Hugo Rizzo <rizzo_hugo@hotmail.com>
2023-10-24 12:12:05 -0300

    Update index4.js

--- index4.js
+++ index4.js
@@ -3,12 +3,18 @@ const teste = {
 
     nome: 'Hugo',
     sobrenome: 'Rizzo',
-    idade: 20
+    idade: 20,
+    endereco: {
+        rua: 'valentim',
+        numero: 222,
+        bairro: 'Random'
+
+    }
 }
 
-const { nome, sobrenome, idade } = teste
+const { nome = 'Não existe', sobrenome, idade, endereco: { rua, numero, bairro }, endereco } = teste
 
-console.log(nome, sobrenome)
+console.log(nome, sobrenome, idade, endereco)
 
 
 
