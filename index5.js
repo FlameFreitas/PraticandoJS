cad7809265934c3cb6e8650b9859c413166c345c
Hugo Rizzo <rizzo_hugo@hotmail.com>
2023-10-26 10:53:28 -0300

    Update index5.js

--- index5.js
+++ index5.js
@@ -33,5 +33,25 @@ do {
 } while (rand !== 10)
 
 
+function soma(x, y) {
+    if (typeof x !== 'number' || typeof y !== 'number') {
+        throw new Error('Ambos os valores devem ser numeros')
+    }
+    return x + y
+}
+
+
+
+try {
+    //Executada quando não há erros
+    console.log(soma(1, 2))
+    console.log(soma('2', 4))
+
+} catch (error) {
+    //Executada quando há erros
+    console.log(error)
+
+}
+
 
 
