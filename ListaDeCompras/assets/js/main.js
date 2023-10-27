7baf8f9bddb7abdb95de2185d5e53be3168fcfba
Hugo Rizzo <rizzo_hugo@hotmail.com>
2023-10-27 14:39:10 -0300

    att

--- ListaDeCompras/assets/js/main.js
+++ ListaDeCompras/assets/js/main.js
@@ -1,9 +1,27 @@
-const lista = document.querySelector('.lista')
+const inputTarefa = document.querySelector('.input-tarefa')
+const buttonWork = document.querySelector('.add-work')
+const listWorks = document.querySelector('.works')
 
-lista.addEventListener('submit', function (event) {
-    event.preventDefault()
+function criaLi() {
+    const li = document.createElement('li')
+    return li
+}
+
+inputTarefa.addEventListener('keypress', function (e) {
+    if (e.keyCode === 13) console.log('Enter press')
 })
 
+function criaTarefa(textInput) {
+    const li = criaLi()
+    li.innerText = textInput
+    listWorks.appendChild(li)
+}
+
+
+buttonWork.addEventListener('click', function (e) {
+    if (!inputTarefa.value) return
+    criaTarefa(inputTarefa.value)
+})
 
 
 
