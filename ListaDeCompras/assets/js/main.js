b3c9e14222c598bf03a07f98d60f286c23072a2d
Hugo Rizzo <rizzo_hugo@hotmail.com>
2023-10-27 15:31:36 -0300

    att

--- ListaDeCompras/assets/js/main.js
+++ ListaDeCompras/assets/js/main.js
@@ -8,13 +8,36 @@ function criaLi() {
 }
 
 inputTarefa.addEventListener('keypress', function (e) {
-    if (e.keyCode === 13) console.log('Enter press')
+    if (e.keyCode === 13) {
+        console.log('Enter press')
+        if (!inputTarefa.value) return
+        criaTarefa(inputTarefa.value)
+    }
 })
+
+function clearInput() {
+    inputTarefa.value = ''
+    inputTarefa.focus()
+
+}
+
+function createDeleteButton(li) {
+    li.innerText += ''
+    const buttonDelete = document.createElement('button')
+    buttonDelete.innerText = 'Delete'
+    li.appendChild(buttonDelete)
+    buttonDelete.setAttribute('class', 'delete')
+    buttonDelete.setAttribute('title', 'deleteThisWork')
+    li.appendChild(buttonDelete)
+}
 
 function criaTarefa(textInput) {
     const li = criaLi()
     li.innerText = textInput
     listWorks.appendChild(li)
+    clearInput()
+    createDeleteButton(li)
+    saveWork()
 }
 
 
@@ -24,4 +47,27 @@ buttonWork.addEventListener('click', function (e) {
 })
 
 
+document.addEventListener('click', function (e) {
+    const el = e.target
+    if (el.classList.contains('delete')) {
+        el.parentElement.remove()
+    }
 
+})
+
+function saveWork() {
+    const liWork = listWorks.querySelectorAll('li')
+    const listOfWorks = []
+
+    let work
+    for (work of liWork) {
+        let workText = work.innerText
+        workText = workText.replace('Delete', '').trim()
+        console.log(workText)
+        listOfWorks.push(workText)
+    }
+
+    const workJSON = JSON.stringfy(listOfWorks)
+    localStorage.setItem('works', workJSON)
+
+}
