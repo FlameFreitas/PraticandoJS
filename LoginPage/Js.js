14a83cb2be58d6702eb8cbb3d8d729e4bd03184d
Hugo Rizzo <rizzo_hugo@hotmail.com>
2023-10-30 13:13:28 -0300

    att

---
+++ LoginPage/Js.js
@@ -0,0 +1,60 @@
+// Cadastro
+
+const botaoEnviar = document.getElementById("botaoEnviar");
+
+botaoEnviar.addEventListener("click", function () {
+    const nome = document.getElementById("nome").value;
+    const email = document.getElementById("email").value;
+    const senha = document.getElementById("senha").value;
+    const sexo = document.getElementById("sexo").value;
+
+    // Verifica se todos os campos estão preenchidos
+    if (!nome || !email || !senha || !sexo) {
+        alert("Preencha todos os campos antes de enviar.");
+        return;
+    }
+
+    const userData = {
+        nome: nome,
+        senha: senha,
+        sexo: sexo
+    };
+
+    // Verifica se a chave (email) já existe no localStorage
+    if (localStorage.getItem(email)) {
+        alert("Este email já foi cadastrado. Use outro email.");
+    } else {
+        // Tenta salvar os dados no localStorage
+        try {
+            localStorage.setItem(email, JSON.stringify(userData));
+            alert("Cadastro realizado com sucesso!");
+        } catch (error) {
+            alert("Ocorreu um erro ao salvar os dados. Tente novamente mais tarde.");
+        }
+    }
+});
+
+
+
+// Login
+
+const botaoLogin = document.getElementById("botaoLogar")
+
+botaoLogin.addEventListener("click", function () {
+    const emailLogin = document.getElementById("emailLogin").value;
+    const senhaLogin = document.getElementById("senhaLogin").value;
+
+    const userData = JSON.parse(localStorage.getItem(emailLogin));
+
+    if (userData) {
+        if (userData.senha === senhaLogin) {
+            alert("Login bem-sucedido!");
+        } else {
+            alert("Senha incorreta. Tente novamente.");
+        }
+    } else {
+        alert("Email não encontrado. Verifique seu email e tente novamente.");
+    }
+
+})
+
