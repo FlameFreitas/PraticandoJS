5d000952f0d6fa8d7ecb214c9ec0584c421cf34a
Hugo Rizzo <rizzo_hugo@hotmail.com>
2023-10-31 15:56:19 -0300

    Create index9.js

---
+++ index9.js
@@ -0,0 +1,13 @@
+//Escopo léxico
+
+//Podemos declarar uma variavel global e chama-la dentro da função que ja dara certo
+const nome = 'Hugo Rizzo'
+
+function falaNome() {
+    console.log(nome)
+}
+
+//Podemos fazer funções compostas tambem
+
+
+
