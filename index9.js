2c7a1fc5232ab99ba39d4a33cdf590b68725328e
Hugo Rizzo <rizzo_hugo@hotmail.com>
2023-10-31 16:00:53 -0300

    Update index9.js

--- index9.js
+++ index9.js
@@ -4,10 +4,21 @@
 const nome = 'Hugo Rizzo'
 
 function falaNome() {
+    //Porém se declararmos uma variavel "nome" dentro da própria função "pai", automaticamente sera executado
+    //a variável que esteja dentro do mesmo
+
+    const nome = 'Mostrando Hugo Rizzo'
     console.log(nome)
 }
 
 //Podemos fazer funções compostas tambem
 
+function apresentarFalaNome() {
+    falaNome()
+}
+
+
+//Ocorrera exatamente o mesmo resultado
+apresentarFalaNome()
 
 
