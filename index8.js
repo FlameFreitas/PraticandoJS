2d208b39319a365834b500369e9f87c57890a509
Hugo Rizzo <rizzo_hugo@hotmail.com>
2023-10-31 15:16:03 -0300

    att

---
+++ index8.js
@@ -0,0 +1,9 @@
+document.addEventListener("click", function () {
+    document.body.style.backgroundColor = 'red'
+    document.body.style.backgroundColor = 'green'
+    document.body.style.backgroundColor = 'blue'
+    document.body.style.backgroundColor = 'yellow'
+    document.body.style.backgroundColor = 'lightBlue'
+
+
+})
