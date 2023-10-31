25236fccac900004416687d83249c6265bf07aed
Hugo Rizzo <rizzo_hugo@hotmail.com>
2023-10-31 15:41:49 -0300

    Update index8.js

--- index8.js
+++ index8.js
@@ -1,10 +1,27 @@
-document.addEventListener("click", function () {
-    document.body.style.backgroundColor = 'red'
-    document.body.style.backgroundColor = 'green'
-    document.body.style.backgroundColor = 'blue'
-    document.body.style.backgroundColor = 'yellow'
-    document.body.style.backgroundColor = 'lightBlue'
+// document.addEventListener("click", function () {
+//     document.body.style.backgroundColor = 'red'
+//     document.body.style.backgroundColor = 'green'
+//     document.body.style.backgroundColor = 'blue'
+//     document.body.style.backgroundColor = 'yellow'
+//     document.body.style.backgroundColor = 'lightBlue'
 
 
-})
+// })
+
+
+function multiplica(multipiicador) {
+    //Recebe multiplicador
+    return function (n) {
+        return n * multipiicador
+    }
+}
+
+const duplica = multiplica(2)
+const triplica = multiplica(3)
+const quadriplica = multiplica(4)
+
+console.log(duplica(2))
+console.log(triplica(3))
+console.log(quadriplica(4))
+
 
