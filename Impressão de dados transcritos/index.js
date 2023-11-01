3c011e2d8d683e5785eca5d29364ccfa1315a8ee
Hugo Rizzo <rizzo_hugo@hotmail.com>
2023-11-01 11:04:48 -0300

    att

--- Impressão de dados transcritos/index.js
+++ Impressão de dados transcritos/index.js
@@ -1,14 +1,20 @@
-const nome = document.getElementById("nome")
-const sobrenome = document.getElementById("sobrenome")
-const idade = document.getElementById("idade")
+const container = document.getElementById("container")
+
+const nome = document.getElementById("nome").value
+const sobrenome = document.getElementById("sobrenome").value
+const idade = document.getElementById("idade").value
 
 function p() {
-    const p = createElement('p')
-
+    var p = document.createElement("p")
     return p
 }
 
+p().textContent = nome
+p().textContent = sobrenome
+p().textContent = idade
 
+
+container.appendChild(p())
 
 
 
