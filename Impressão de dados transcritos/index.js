621331ebf1ce719988c1d61fdfa2625d7584c490
Hugo Rizzo <rizzo_hugo@hotmail.com>
2023-10-31 21:15:25 -0300

    Update index.js

--- Impressão de dados transcritos/index.js
+++ Impressão de dados transcritos/index.js
@@ -2,6 +2,13 @@ const nome = document.getElementById("nome")
 const sobrenome = document.getElementById("sobrenome")
 const idade = document.getElementById("idade")
 
+function p() {
+    const p = createElement('p')
+
+    return p
+}
+
+
 
 
 
