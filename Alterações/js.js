fb8809e55bdcbef6f2bed02f52ebcf2d85f4cec8
Hugo Rizzo <rizzo_hugo@hotmail.com>
2023-11-01 13:02:17 -0300

    att

---
+++ Alterações/js.js
@@ -0,0 +1,19 @@
+const texto1 = "Texto 1"
+const texto2 = "Texto 1"
+const texto3 = "Texto 1"
+const texto4 = "Texto 1"
+const texto5 = "Texto 1"
+const texto6 = "Texto 1"
+
+
+var alteraTexto = [texto1, texto2, texto3, texto4, texto5, texto6]
+
+alteraTexto[0] = "Novo texto 1"
+alteraTexto[1] = "Novo texto 2"
+alteraTexto[2] = "Novo texto 3"
+alteraTexto[3] = "Novo texto 4"
+alteraTexto[4] = "Novo texto 5"
+alteraTexto[5] = "Novo texto 6"
+
+
+console.log(alteraTexto)
