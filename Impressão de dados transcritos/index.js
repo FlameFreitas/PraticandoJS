08a86cdb48f61cb1d8614b23668a975114840690
Hugo Rizzo <rizzo_hugo@hotmail.com>
2023-11-01 11:29:18 -0300

    att

--- Impressão de dados transcritos/index.js
+++ Impressão de dados transcritos/index.js
@@ -1,20 +1,25 @@
 const container = document.getElementById("container")
+const formulario = document.getElementById("formulario")
 
-const nome = document.getElementById("nome").value
-const sobrenome = document.getElementById("sobrenome").value
-const idade = document.getElementById("idade").value
-
-function p() {
-    var p = document.createElement("p")
-    return p
-}
-
-p().textContent = nome
-p().textContent = sobrenome
-p().textContent = idade
+const dados = document.getElementById("submit").value
 
 
-container.appendChild(p())
 
+
+
+
+formulario.addEventListener("submit", function (event) {
+    event.preventDefault()
+
+    const nome = document.getElementById("nome").value
+    const sobrenome = document.getElementById("sobrenome").value
+    const idade = document.getElementById("idade").idade
+
+    const p = document.createElement("p")
+    p.textContent = `Nome: ${nome}, Sobrenome: ${sobrenome}, Idade: ${idade}`
+
+    container.appendChild(p)
+
+})
 
 
