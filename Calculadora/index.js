96b603c56e46368519a555c921a75e9e4e789865
Hugo Rizzo <rizzo_hugo@hotmail.com>
2023-11-02 12:36:53 -0300

    att

--- Calculadora/index.js
+++ Calculadora/index.js
@@ -1,29 +1,36 @@
-const numero1 = parseFloat(document.getElementById("numero1").value)
-const numero2 = parseFloat(document.getElementById("numero2").value)
-const operacao = document.getElementById("operacao").value
-const resultado = document.getElementById("resultado")
-
+const numero1 = parseFloat(document.getElementById("numero1").value);
+const numero2 = parseFloat(document.getElementById("numero2").value);
+const operacao = document.getElementById("operacao").value;
+const resultado = document.getElementById("resultado");
 
 function calculo() {
+    var result;
 
-    var result
-
-    if (operacao == "+") {
+    if (operacao === "+") {
         result = numero1 + numero2;
-    } else if (operacao == "-") {
-        result = numero1 - numero2
-    } else if (operacao == "/") {
-        result = numero1 / numero2
-    } else if (operacao == "*") {
-        result = numero1 * numero2
+    } else if (operacao === "-") {
+        result = numero1 - numero2;
+    } else if (operacao === "/") {
+        result = numero1 / numero2;
+    } else if (operacao === "*") {
+        result = numero1 * numero2;
+    } else {
+        // Lidar com operação inválida ou não selecionada
+        result = "Operação inválida ou não selecionada";
     }
 
+    return result;
 }
 
 function resultadoFinal() {
+    const result = calculo();
 
-    calculo()
-    resultado.innerHTML += result
+    // Verificar se o resultado não é uma mensagem de erro
+
+    if (typeof result === "number") {
+        resultado.innerHTML = "Resultado: " + result;
+    } else {
+        // Exibir mensagem de erro
+        resultado.innerHTML = result;
+    }
 }
-
-
