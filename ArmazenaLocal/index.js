f541d4fd7dc4280bc9f1281a6d1fbf00d9e555b1
Hugo Rizzo <rizzo_hugo@hotmail.com>
2023-11-03 15:09:16 -0300

    Update index.js

--- ArmazenaLocal/index.js
+++ ArmazenaLocal/index.js
@@ -1,14 +1,24 @@
 
-const enviaDados = document.getElementById("enviaDados")
+const enviaDados = document.getElementById("enviaDados");
+
+enviaDados.addEventListener("click", function () {
+    const nome = document.getElementById("nome").value;
+    const sobrenome = document.getElementById("sobrenome").value;
+    const idade = document.getElementById("idade").value;
+    const CPF = document.getElementById("CPF").value;
+
+    //Vamos verificar se os dados estão preencidos
+
+    if (!nome || !sobrenome || !idade || !CPF) {
+
+        alert("Por favor preencha todos os campos fornecidos");
+        return;
+    }
 
 
-const nome = document.getElementById("nome").value
-const sobrenome = document.getElementById("sobrenome").value
-const idade = document.getElementById("idade").value
-const CPF = document.getElementById("CPF").value
 
 
-
+})
 
 
 
