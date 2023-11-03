4b4b985e72efaa04765f6e52691340e7497200d9
Hugo Rizzo <rizzo_hugo@hotmail.com>
2023-11-03 14:52:46 -0300

    att

---
+++ ArmazenaLocal/index.js
@@ -0,0 +1,17 @@
+
+const enviaDados = document.getElementById("enviaDados")
+
+
+const nome = document.getElementById("nome").value
+const sobrenome = document.getElementById("sobrenome").value
+const idade = document.getElementById("idade").value
+const CPF = document.getElementById("CPF").value
+
+
+
+
+
+
+
+
+
