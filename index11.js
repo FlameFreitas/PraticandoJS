4b802ecf63fcde24b85a49678ceeb8ce20ad0abb
Hugo Rizzo <rizzo_hugo@hotmail.com>
2023-11-09 12:26:05 -0300

    att

---
+++ index11.js
@@ -0,0 +1,14 @@
+(function criaNome(nome, sobrenome, idade) {
+
+    const dados = {
+        nome: nome,
+        sobrenome: sobrenome,
+        idade: idade
+    }
+
+    console.log(dados)
+
+})
+
+console.log(nome = 'Hugo', sobrenome = 'Rizzo', idade = 20)
+
