ff6b1189d68b9ba0e9b3afcb2e1f4d8cf9b786ed
Hugo Rizzo <rizzo_hugo@hotmail.com>
2023-11-09 13:02:05 -0300

    Update index12.js

--- index12.js
+++ index12.js
@@ -21,7 +21,7 @@ function criaPessoa(nome, sobrenome, idade, p, a) {
 const p1 = criaPessoa('Hugo', 'Rizzo', 20, 80, 1.80)
 const p2 = criaPessoa('Felipe', 'Rizzo', 25, 75, 1.70)
 
-console.log(p1.fala('sobre JS'))
+console.log(p2.imc())
 
 
 
