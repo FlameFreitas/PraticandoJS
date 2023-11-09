35832f8080b49d12bdb492e6e85e5c4d04f1e8d7
Hugo Rizzo <rizzo_hugo@hotmail.com>
2023-11-09 13:14:44 -0300

    Update index12.js

--- index12.js
+++ index12.js
@@ -4,12 +4,21 @@ function criaPessoa(nome, sobrenome, idade, p, a) {
         nome: nome,
         sobrenome: sobrenome,
         idade, idade,
+
+        get nomeCompleto() {
+            return `${this.nome} ${this.sobrenome}`;
+        },
+        set nomeCompleto(valor) {
+            valor = valor.split(' ');
+            this.nome = valor.shift();
+            this.sobrenome = valor.join(' ')
+        },
         fala(assunto) {
             console.log(`${nome} esta falando ${assunto}`)
         },
         peso: p,
         altura: a,
-        imc() {
+        get imc() {
             const indice = this.peso / (this.altura ** 2);
             return indice.toFixed(2);
 
@@ -20,9 +29,12 @@ function criaPessoa(nome, sobrenome, idade, p, a) {
 
 const p1 = criaPessoa('Hugo', 'Rizzo', 20, 80, 1.80)
 const p2 = criaPessoa('Felipe', 'Rizzo', 25, 75, 1.70)
-
 console.log(p2.imc())
 
+p1.nomeCompleto = 'Tais Rizzo'
+
+console.log(p1.nome)
+console.log(p1.sobrenome)
 
 
 
